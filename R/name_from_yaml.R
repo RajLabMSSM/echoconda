d5037c8be77aee7d2bfa8e22477dbe1c57fc11d9
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-05-27 15:40:12 +0100

    get name from yaml

--- R/name_from_yaml.R
+++ R/name_from_yaml.R
@@ -4,15 +4,9 @@
 #' @keywords internal
 #' @importFrom yaml read_yaml
 name_from_yaml <- function(yaml_path,
-                           verbose = TRUE) {
-    
-    if(!file.exists(yaml_path)){
-        # messager(yaml_path,"does not exist.",
-        #          "Inferring conda_env name from path.",v=verbose)
-        conda_env <- gsub(".yaml$|.yml$","",basename(yaml_path))
-        return(conda_env)
-    }
-    messager("Retrieving conda env name from yaml:",yaml_path, v = verbose)
+                           verbose = TRUE) { 
+    messager("Retrieving conda env name from yaml:",
+             yaml_path, v = verbose)
     conda_env <- yaml::read_yaml(yaml_path)$name 
     return(conda_env)
 }
